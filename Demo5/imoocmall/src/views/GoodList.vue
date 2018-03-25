de3c07e8e3b5ad67ff00b15dfdbeae3880cdd4f6
CcisDevil <q296744594@163.com>
2018-03-25 17:38:34 +0800

    创建vue demo5
    npm i vue --save

---
+++ Demo5/imoocmall/src/views/GoodList.vue
@@ -0,0 +1,10 @@
+<template>
+            <div>这是商品列表页面
+            </div>
+</template>
+<style>
+
+</style>
+<script>
+
+</script>
---
+++ index.js
@@ -0,0 +1,11 @@
+function sum(a,b) {
+    return a+b;
+}
+var s= sum(10,8);
+console.log("sum:"+s)
+
+function name() {
+    return function b(){
+        console.log(a);
+    }
+}
